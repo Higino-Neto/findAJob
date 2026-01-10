1df810f76623e6875c295c66de5febd07692e972
Tawl <tawl.tack@gmail.com>
2026-01-10 18:05:26 -0300

    Add PostJob page and UI designed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import NavBar from "@/components/NavBar";
+import { Providers } from "@/components/Providers";
+import "./globals.css";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -11,10 +10,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        <div className="min-h-screen bg-gray-50">
-          <NavBar />
-          <main className="container mx-auto px-4 py-8">{children}</main>
-        </div>
+        <Providers>
+          <div className="min-h-screen bg-gray-50">
+            <NavBar />
+            <main className="container mx-auto px-4 py-8">{children}</main>
+          </div>
+        </Providers>
       </body>
     </html>
   );
