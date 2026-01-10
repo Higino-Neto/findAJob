80928142da32f65a28a1f07e80cccfdd1663dc24
Tawl <tawl.tack@gmail.com>
2026-01-10 19:44:42 -0300

    Add postJob page syncronized with database

---
+++ app/api/jobs/route.ts
@@ -0,0 +1,27 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/authConfig";
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user)
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+
+  try {
+    const data = await request.json();
+
+    const job = await prisma.job.create({
+      data: {
+        ...data,
+        postedById: session.user.id as string,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.error("Error creating new Job: ", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
