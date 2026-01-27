dd4c4e6b8a2f355264689e2f05cc4c1dd377008e
Tawl <tawl.tack@gmail.com>
2026-01-27 18:20:28 -0300

    feat: add delete endpoint for application

--- app/api/applications/[id]/route.ts
+++ app/api/applications/[id]/route.ts
@@ -28,25 +28,25 @@ export async function PATCH(
   }
 }
 
-// export async function DELETE(
-//   request: Request,
-//   { params }: { params: Promise<{ id: string }> },
-// ) {
-//   const session = await getServerSession(authOptions);
-//   if (!session) {
-//     return NextResponse.redirect(new URL("auth/signin", request.url));
-//   }
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.redirect(new URL("auth/signin", request.url));
+  }
 
-//   try {
-//     const { id } = await params;
+  try {
+    const { id } = await params;
 
-//     const deletedApplication = await prisma.application.delete({
-//       where: { id },
-//     });
+    const deletedApplication = await prisma.application.delete({
+      where: { id },
+    });
 
-//     return NextResponse.json(deletedApplication);
-//   } catch (error) {
-//     console.error("Error deleting application: ", error);
-//     return new NextResponse("Internal Server Error", { status: 500 });
-//   }
-// }
+    return NextResponse.json(deletedApplication);
+  } catch (error) {
+    console.error("Error deleting application: ", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
