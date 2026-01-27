572eab89d8f40a321f6d4a85cbfb960c714ad66a
Tawl <tawl.tack@gmail.com>
2026-01-26 21:20:54 -0300

    feat: add route handler to download curriculum pdf

---
+++ app/api/applications/download-curriculum/[...curriculumPath]/route.ts
@@ -0,0 +1,30 @@
+import { authOptions } from "@/authConfig";
+import { supabaseAdmin } from "@/lib/supabase-admin";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ curriculumPath: string[] }> },
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+  try {
+    const { curriculumPath } = await params;
+    const [userIdPath, jobIdPath] = curriculumPath;
+    console.log(curriculumPath);
+
+    const { data, error } = await supabaseAdmin.storage
+      .from("curriculum")
+      .download(`${userIdPath}/${jobIdPath}`);
+    if (error) {
+      throw new Error("Error fetching from Object Storage: ", error);
+    }
+    return new NextResponse(data);
+  } catch (error) {
+    console.error("Error getting curriculum from Object Storage");
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
