fe9f44c4e49e9334d72e736f8d891bd3a311e73e
Tawl <tawl.tack@gmail.com>
2026-01-28 22:34:31 -0300

    chore: change return structure on component sweper

--- components/sweper.tsx
+++ components/sweper.tsx
@@ -11,29 +11,31 @@ type JobProps = {
 
 export function SweperJobs({ jobs, setSelectedJob }: JobProps) {
   return (
-    <Swiper
-      spaceBetween={15}
-      slidesPerView={5.5}
-      modules={[Navigation]}
-      navigation
-    >
-      {jobs.map((job) => {
-        return (
-          <SwiperSlide key={job.id}>
-            <button
-              onClick={() => setSelectedJob(job)}
-              className="w-full text-start hover:cursor-pointer rounded bg-white shadow p-2"
-            >
-              <div className="">
-                <h1>{job.title}</h1>
-                <p>{job.company}</p>
-                <p>{job.type}</p>
-                <p>{job.salary}</p>
-              </div>
-            </button>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <>
+      <Swiper
+        spaceBetween={15}
+        slidesPerView={5.5}
+        modules={[Navigation]}
+        navigation
+      >
+        {jobs.map((job) => {
+          return (
+            <SwiperSlide key={job.id}>
+              <button
+                onClick={() => setSelectedJob(job)}
+                className="w-full text-start hover:cursor-pointer rounded bg-white shadow p-2"
+              >
+                <div className="">
+                  <h1>{job.title}</h1>
+                  <p>{job.company}</p>
+                  <p>{job.type}</p>
+                  <p>{job.salary}</p>
+                </div>
+              </button>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 }
