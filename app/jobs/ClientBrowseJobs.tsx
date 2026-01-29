61e49c6f9639d81530cb525196db70e1ad7004f0
Tawl <tawl.tack@gmail.com>
2026-01-28 22:06:46 -0300

    feat: Add client-side layer to block user from applying to the same job multiple times

--- app/jobs/ClientBrowseJobs.tsx
+++ app/jobs/ClientBrowseJobs.tsx
@@ -1,10 +1,27 @@
 "use client";
+import ApplyButton from "@/components/jobs/ApplyButton";
 import { Pagination } from "@/components/Pagination";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+// type Job = {
+//   id: string;
+//   type: string;
+//   title: string;
+//   company: string;
+//   location: string;
+//   description: string;
+//   salary: string | null;
+//   postedAt: Date;
+//   postedById: string;
+// };
+
 type Job = {
+  applications: {
+    userId: string;
+  }[];
+} & {
   id: string;
   type: string;
   title: string;
@@ -70,18 +87,18 @@ export default function ClientBrowseJobs({ page }: { page: number }) {
           totalPages={totalPages}
           onPageChange={onPageChange}
         />
-
       </div>
       {showJob && (
         <div className=" w-1/2 flex flex-col border border-gray-300 rounded-lg shadow-lg">
           <div className="flex flex-col h-screen text-start bg-white p-4 rounded-lg">
             <a className="text-2xl">{showJob.title}</a>
-            <button
+            {/* <button
               onClick={() => router.push(`/jobs/${showJob.id}/applications`)}
               className="bg-indigo-500 w-40 px-2 py-1 rounded-md shadow-md mt-6 mb-3 text-gray-50 hover:cursor-pointer"
             >
               Apply to this job
-            </button>
+            </button> */}
+            <ApplyButton jobId={showJob.id} hasApplied={showJob.applications.length > 0} />
             <a className="">{showJob.type}</a>
             <a className="">{showJob.company}</a>
             <a className="">{showJob.location}</a>
